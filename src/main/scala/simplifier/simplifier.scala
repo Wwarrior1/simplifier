524f9b83b13283ccd5440669ed80a8bedc6ebad5
Wojciech Baczyński <baczynskiwojtek@gmail.com>
2017-01-07 16:53:36 +0100

    Initial commit. Template from academic project resources.

---
+++ src/main/scala/simplifier/simplifier.scala
@@ -0,0 +1,12 @@
+package simplifier
+
+import AST._
+
+// to implement
+// avoid one huge match of cases
+// take into account non-greedy strategies to resolve cases with power laws
+object Simplifier {
+
+  def simplify(node: Node) = node
+
+}
